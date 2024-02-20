7514a057dc46ae137856038280dbc65c69027154
jrk <jrkpetiq@gmail.com>
2024-02-19 20:01:34 -0700

    through Leveled logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,29 +1,38 @@
 package main
 
 import (
-    "log"
-    "net/http"
+	"flag"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
-    mux := http.NewServeMux()
+	addr := flag.String("addr", ":4000", "HTTP network address")
 
-    // Create a file server which serves files out of the "./ui/static" directory.
-    // Note that the path given to the http.Dir function is relative to the project
-    // directory root.
-    fileServer := http.FileServer(http.Dir("./ui/static/"))
+	flag.Parse()
 
-    // Use the mux.Handle() function to register the file server as the handler for
-    // all URL paths that start with "/static/". For matching paths, we strip the
-    // "/static" prefix before the request reaches the file server.
-    mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime|log.LUTC)
 
-    // Register the other application routes as normal.
-    mux.HandleFunc("/", home)
-    mux.HandleFunc("/snippet/view", snippetView)
-    mux.HandleFunc("/snippet/create", snippetCreate)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-    log.Print("Starting server on :4000")
-    err := http.ListenAndServe(":4000", mux)
-    log.Fatal(err)
+	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
+    srv := &http.Server{
+        Addr: *addr,
+        ErrorLog: errorLog,
+        Handler: mux,
+    }
+
+	infoLog.Printf("Starting server on %s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
