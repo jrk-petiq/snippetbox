c7bc8388c57aa84b472ad2c7307894d2eecd267c
John Kowalsky <john.kowalsky@petiq.com>
2024-03-19 08:36:22 -0600

    through Template actions and functions

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,5 +3,6 @@ package main
 import "github.com/jrk-petiq/snippetbox/internal/models"
 
 type templateData struct {
-	Snippet *models.Snippet
+	Snippet  *models.Snippet
+	Snippets []*models.Snippet
 }
