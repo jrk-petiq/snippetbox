36d9a8994576e7576104d4ec6bbca3a375b6e1cf
John Kowalsky <john.kowalsky@petiq.com>
2024-03-26 09:32:33 -0600

    through Custom template functions

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -15,7 +15,7 @@ type templateData struct {
 }
 
 func humanDate(t time.Time) string {
-	return t.Format("02 Jan 2023 at 15:04")
+	return t.Format("02 Jan 2006 at 15:04")
 }
 
 var functions = template.FuncMap{
